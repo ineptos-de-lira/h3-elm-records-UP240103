7245a298761c07b8b4113f0490853f858db56479
unknown <up240103@alumnos.upa.edu.mx>
2026-02-13 20:59:50 -0600

    Agrego contenido a Helper.elm

--- src/Helper.elm
+++ src/Helper.elm
@@ -1,1 +1,95 @@
-module Helper exposing (..)
+module Main exposing (..)
+
+import Html exposing (Html, div, h1, li, text, ul)
+
+
+add2 : Int -> Int -> Int
+add2 x y =
+    x + y
+
+
+add3 : Float -> Float -> Float -> Float
+add3 x y z =
+    x + y + z
+
+
+calc : Int -> Int -> (Int -> Int -> Int) -> Int
+calc x y op =
+    op x y
+
+
+type alias ProgrammingLanguage =
+    { name : String
+    , releaseYear : Int
+    , currentVersion : String
+    }
+
+
+languageNames : List ProgrammingLanguage -> List String
+languageNames list =
+    List.map .name list
+
+
+type alias User =
+    { name : String
+    , uType : String
+    }
+
+
+onlyStudents : List User -> List String
+onlyStudents list =
+    List.map
+        (\u ->
+            case u.uType of
+                "Student" ->
+                    u.name
+
+                _ ->
+                    ""
+        )
+        list
+
+
+type Videogame
+    = Videogame String Int Bool Int (List String)
+
+
+getVideogameGenres : List Videogame -> List (List String)
+getVideogameGenres list =
+    List.map
+        (\(Videogame _ _ _ _ genres) ->
+            genres
+        )
+        list
+
+
+type alias Computer =
+    { ram : String
+    , model : String
+    , brand : String
+    , screenSize : String
+    }
+
+
+myLaptop : Computer
+myLaptop =
+    { ram = "12GB"
+    , model = "AMD A12"
+    , brand = "HP"
+    , screenSize = "15.6"
+    }
+
+
+main : Html msg
+main =
+    div []
+        [ h1 [] [ text "My laptop" ]
+        , div []
+            [ ul []
+                [ li [] [ text ("Ram: " ++ myLaptop.ram) ]
+                , li [] [ text ("Modelo: " ++ myLaptop.model) ]
+                , li [] [ text ("Marca: " ++ myLaptop.brand) ]
+                , li [] [ text ("Pulgadas: " ++ myLaptop.screenSize) ]
+                ]
+            ]
+        ]
